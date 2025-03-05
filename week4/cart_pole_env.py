dfa70170cf7ec0d745a9f360457214fb53119e01
gabeabenavides2 <156029436+gabeabenavides2@users.noreply.github.com>
2025-03-04 21:24:54 -0500

    Delete week4 directory

--- week4/cart_pole_env.py
+++
@@ -1,7 +0,0 @@
-# cart_pole_env.py
-import gym
-from gym.wrappers import FlattenObservation
-
-def create_env(render_mode=None):
-    env = gym.make('CartPole-v1', render_mode=render_mode)
-    return FlattenObservation(env)
